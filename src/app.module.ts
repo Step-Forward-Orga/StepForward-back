c97c9830340828514f08690bfe841cf7e06318ff
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2024-12-09 16:09:59 +0100

    build(config): config files, updated readme for build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,12 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { TrainingNoteController } from './training-note/training-note.controller';
 import { TrainingNoteModule } from './training-note/training-note.module';
-import { ExerciceService } from './exercice/exercice.service';
-import { ExerciceModule } from './exercice/exercice.module';
 import { ExerciceModule } from './exercice/exercice.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthenticationModule, TrainingNoteModule, ExerciceModule],
   controllers: [AppController],
-  providers: [AppService, ExerciceService],
+  providers: [AppService],
 })
 export class AppModule {}
