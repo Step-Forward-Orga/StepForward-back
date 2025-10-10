7cee2c459604f6974f26f71b8f7c3c8019dbade0
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2025-10-10 22:09:36 +0200

    del: javascript files that were generated

--- src/authentication/enums/JwtType.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JwtType = void 0;
-var JwtType;
-(function (JwtType) {
-    JwtType[JwtType["ACCESS"] = 0] = "ACCESS";
-    JwtType[JwtType["REFRESH"] = 1] = "REFRESH";
-})(JwtType || (exports.JwtType = JwtType = {}));
--- src/workout/entities/workout.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WorkoutPlan = void 0;
-var WorkoutPlan = /** @class */ (function () {
-    function WorkoutPlan() {
-    }
-    return WorkoutPlan;
-}());
-exports.WorkoutPlan = WorkoutPlan;
