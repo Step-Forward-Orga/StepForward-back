52dcd3882052667800952fd558578f05e2b88122
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2025-10-27 18:53:14 +0100

    add: dto to create workout program

--- src/workout-program/dto/create-workout-program.dto.ts
+++ src/workout-program/dto/create-workout-program.dto.ts
@@ -1,1 +1,9 @@
-export class CreateWorkoutProgramDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateWorkoutProgramDto {
+    @ApiProperty({ example: "Beginner Program" })
+    title: string;
+
+    @ApiProperty({ example: "A program designed for beginners to build foundational strength." })
+    description: string
+}
