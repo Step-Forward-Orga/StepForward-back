47b21d91b20ecdae60e0c7e4e84042c8d5caebdc
Antweneee <antweneee.dev@gmail.com>
2026-03-10 13:50:42 +0100

    add: seeding for every object of the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -51,7 +51,7 @@ const seedExercises = async () => {
         {name : "Sited Calf Raise", description: ""},
         {name : "Incline Smith Machine Chest Press", description: ""},
         {name : "Decline Dumbbell Chest Press", description: ""},
-        {name : "Lats Pullover", description: ""},
+        {name : "Lat Pullover", description: ""},
         {name : "Romanian DeadLift (RDL)", description: ""},
         {name : "Leg Press", description: ""},
         {name : "Horizontal Press Calf Extensions", description: ""},
@@ -73,6 +73,15 @@ const seedExercises = async () => {
         {name : "Traction Supination", description: ""},
         {name : "Chin Rows", description: ""},
         {name : "Triceps Overhead Extension", description: ""},
+        {name : "Decline Hammer Strength Uni", description: ""},
+        {name : "High Pec Cable Fly", description: ""},
+        {name : "Forearms", description: ""},
+        {name : "Reverse Crunch", description: ""},
+        {name : "Lombert", description: ""},
+        {name : "Curl", description: ""},
+        {name : "Deadlift", description: ""},
+        {name : "Cable Rear Delt", description: ""},
+        {name : "Bulgarian Split Squat", description: ""},
     ]
 
     for (const exercise of exercises) {
@@ -87,26 +96,228 @@ const seedExercises = async () => {
     }
 }
 
-// const seedWorkout = async () => {
-//     const workouts = [
-//         { title: "Upper 1", description: "Upper 1 workout of the 11th Plan", userId: 1}
-//     ]
+const seedWorkout = async () => {
+    const workouts = [
+        { title: "Upper 1", description: "Upper 1 workout of the 11th Plan", userId: 1 },
+        { title: "Lower 1", description: "Lower 1 workout of the 11th Plan", userId: 1 },
+        { title: "Upper 1", description: "Upper 1 workout of the 11th Plan", userId: 1 },
+        { title: "Lower 2", description: "Lower 2 workout of the 11th Plan", userId: 1 },
+        { title: "Upper 3", description: "Upper 3 workout of the 11th Plan", userId: 1 },
+        { title: "Upper", description: "Upper workout of the 10th Plan", userId: 1 },
+        { title: "Lower", description: "Lower workout of the 10th Plan", userId: 1 },
+        { title: "Upper", description: "Upper workout of the 10th Plan", userId: 1 },
+        { title: "Lower", description: "Lower workout of the 10th Plan", userId: 1 },
+        { title: "Upper", description: "Upper workout of the 10th Plan", userId: 1 },
+    ]
 
-//     for (const workout of workouts) {
-//         await prisma.exercise.upsert({
-//             where: { name: workout.name },
-//             update: {},
-//             create: {
-//                 name: workout.name,
-//                 description: workout.description,
-//             },
-//         })
-//     }
-// }
+    for (const workout of workouts) {
+
+        const createdWorkout = await prisma.workout.create({
+            data: {
+                title: workout.title,
+                description: workout.description,
+                userId: workout.userId,
+                code: "",
+            }
+        })
+
+        const slug = workout.title
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9]+/g, '-')
+        .replace(/^-+|-+$/g, '')
+
+        const code = `${slug || 'workout'}-${createdWorkout.id}`
+
+        await prisma.workout.update({
+        where: { id: createdWorkout.id },
+        data: { 
+            code: code,
+            user: {
+                connect: { id: workout.userId }
+            }}
+        })
+    }
+}
+
+const seedWorkoutProgram = async () => {
+    const workoutPrograms = [
+        { title: "5*W ~ 3 Upper, 2 Lower ~ Plan 10", description: "Upper Lower 5 times a week", userId: 1, workoutsId: [1, 2, 3, 4, 5] },
+        { title: "5*W ~ 3 Upper, 2 Lower ~ Plan 11", description: "Upper Lower 5 times a week", userId: 1, workoutsId: [6, 7, 8, 9, 10] },
+    ]
+
+    for (const workoutProgram of workoutPrograms) {
+        await prisma.workoutProgram.create({
+            data: {
+                title: workoutProgram.title,
+                description: workoutProgram.description,
+                user: { 
+                    connect: { id: workoutProgram.userId }
+                },
+                workouts: {
+                    connect: workoutProgram.workoutsId.map((id) => ({ id })),
+                }
+            },
+        })
+    }
+
+}
+
+const seedWorkoutExercise = async () => {
+    const workoutExercises = [
+        //sm incline press
+        { exerciseId: 20, restTime: "PTM3M",  completed: false, workoutId: 1, 
+            plannedSets: [
+                { reps: 8, weight: 80, order: 1, type: "PLANNED"},
+                { reps: 6, weight: 80, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 65, order: 3, type: "PLANNED"},
+            ]
+        },
+        //lat pull down
+        { exerciseId: 14, restTime: "PTM3M",  completed: false, workoutId: 1,
+            plannedSets: [
+                { reps: 8, weight: 105, order: 1, type: "PLANNED"},
+                { reps: 6, weight: 105, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 90, order: 3, type: "PLANNED"},
+            ]
+        },
+        //decline db press
+        { exerciseId: 21, restTime: "PTM3M",  completed: false, workoutId: 1,
+            plannedSets: [
+                { reps: 7, weight: 48, order: 1, type: "PLANNED"},
+                { reps: 5, weight: 48, order: 2, type: "PLANNED"},
+                { reps: 7, weight: 42, order: 3, type: "PLANNED"},
+            ]
+        },
+        //tbar
+        { exerciseId: 13, restTime: "PTM3M",  completed: false, workoutId: 1,
+            plannedSets: [
+                { reps: 7, weight: 90, order: 1, type: "PLANNED"},
+                { reps: 6, weight: 90, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 75, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Machine pec fly
+        { exerciseId: 11, restTime: "PTM3M",  completed: false, workoutId: 1,
+            plannedSets: [
+                { reps: 8, weight: 40, order: 1, type: "PLANNED"},
+                { reps: 5, weight: 40, order: 2, type: "PLANNED"},
+                { reps: 6, weight: 30, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Lat pullover
+        { exerciseId: 22, restTime: "PTM3M",  completed: false, workoutId: 1,
+            plannedSets: [
+                { reps: 6, weight: 32.5, order: 1, type: "PLANNED"},
+                { reps: 5, weight: 32.5, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 25, order: 3, type: "PLANNED"},
+            ]
+        },
+        // ! workout 2
+        //Bulgarian Split Squat
+        { exerciseId: 52, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 8, weight: 80, order: 1, type: "PLANNED"},
+                { reps: 6, weight: 80, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 65, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Sited Leg Extension
+        { exerciseId: 7, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 12, weight: 100, order: 1, type: "PLANNED"},
+                { reps: 12, weight: 100, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 85, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Leg Press
+        { exerciseId: 24, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 8, weight: 220, order: 1, type: "PLANNED"},
+                { reps: 6, weight: 220, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 190, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Sited Leg Curl
+        { exerciseId: 8, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 10, weight: 75, order: 1, type: "PLANNED"},
+                { reps: 8, weight: 75, order: 2, type: "PLANNED"},
+                { reps: 10, weight: 60, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Horizontal Press Calf Extensions
+        { exerciseId: 25, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 15, weight: 200, order: 1, type: "PLANNED"},
+                { reps: 13, weight: 200, order: 2, type: "PLANNED"},
+                { reps: 15, weight: 180, order: 3, type: "PLANNED"},
+            ]
+        },
+        //Cable Crunch
+        { exerciseId: 40, restTime: "PTM3M",  completed: false, workoutId: 2,
+            plannedSets: [
+                { reps: 15, weight: 45, order: 1, type: "PLANNED"},
+                { reps: 12, weight: 45, order: 2, type: "PLANNED"},
+                { reps: 15, weight: 40, order: 3, type: "PLANNED"},
+            ]
+        },
+    ]
+
+    for (const workoutExercise of workoutExercises) {
+    await prisma.workoutExercise.create({
+        data: {
+            exercise: {
+                connect: { id: workoutExercise.exerciseId }
+            },
+            restTime: workoutExercise.restTime,
+            completed: workoutExercise.completed,
+            workout: {
+                connect: { id: workoutExercise.workoutId }
+            },
+            plannedSets: {
+                create: workoutExercise.plannedSets.map((set) => ({
+                    reps: set.reps,
+                    weight: set.weight,
+                    order: set.order,
+                    type: set.type as "PLANNED" | "COMPLETED",
+                })),
+            },
+        },
+    })
+    }
+}
+
+const seedNotes = async () => {
+    const notes = [
+        { userId: 1, note: "control ++",  workoutExerciseId: 4 },
+        { userId: 1, note: "fst set meh ",  workoutExerciseId: 4 },
+        { userId: 1, note: "stop the low foot shit just go regular (no rest between sets) ",  workoutExerciseId: 9 },
+        { userId: 1, title: "TODO", note: "keep this program for at least a good 5 months",  workoutProgramId: 2 },
+        { userId: 1, title: "Focus", note: "Focus on lifting heavy weight for 5-10 reps then up the weight",  workoutId: 1 },
+    ]
+
+    for (const note of notes) {
+        await prisma.notes.create({
+            data: {
+                title: note.title,
+                note: note.note,
+                user: { connect: { id: note.userId } },
+                workout: note.workoutId ? { connect: { id: note.workoutId } } : undefined,
+                workoutProgram: note.workoutProgramId ? { connect: { id: note.workoutProgramId } } : undefined,
+                workoutExercise: note.workoutExerciseId ? { connect: { id: note.workoutExerciseId } } : undefined,
+            },
+        })
+    }
+}
 
 async function main() {
-    seedUsers();
-    seedExercises();
+    await seedUsers();
+    await seedExercises();
+    await seedWorkout();
+    await seedWorkoutProgram();
+    await seedWorkoutExercise();
+    await seedNotes();
 }
 
 main()
